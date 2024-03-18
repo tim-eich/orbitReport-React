82baf4536a9d15db3162b1f23353317b32a8d003
Tim-E <caffeinated.ink@gmail.com>
2024-03-17 21:27:22 -0400

    2 tests passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/banner";
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+function Banner() {
+    return `
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+
+        `;
+}
+
+export default Banner;
