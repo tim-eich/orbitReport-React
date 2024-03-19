f1be795f60d4446a120a9215c70f9796fbec28ac
Tim-E <caffeinated.ink@gmail.com>
2024-03-19 12:30:40 -0400

    correct typo on Banner import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-import Banner from "./components/banner";
+import Banner from "./components/Banner";
 import { useState } from "react";
 import satData from "./components/satData";
 
