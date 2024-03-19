47b677852d833a62e1a40d3469bcde51a377597e
Tim-E <caffeinated.ink@gmail.com>
2024-03-19 12:17:19 -0400

    all tests passing!

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,16 +2,26 @@ const Table = ({ sat }) => {
   return (
     <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+    <tr>
+      <th>Name</th>
+      <th>Type of Satellite</th>
+      <th>Launch Date</th>
+      <th>Status</th>
+    </tr>
+    </thead>
+    <tbody>
+      { sat.map(( data, id ) => {
+        return(
+          <tr key ={ sat.id }>
+            <td key ={ sat.id} >{ data.name }</td>
+            <td key ={ sat.id} >{ data.type }</td>
+            <td key ={ sat.id} >{ data.launchDate }</td>
+            { sat.operational ? <td key ={ sat.id} >Active</td> : <td key ={ sat.id} >Inactive</td> }
+          </tr>
+        )
+      })}
+    </tbody>
+  </table>
   );
 };
 
