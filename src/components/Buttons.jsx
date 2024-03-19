dca993d879219e184c16245e62d0cebee5f3bdd6
Tim-E <caffeinated.ink@gmail.com>
2024-03-19 11:07:40 -0400

    5 tests passing

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,10 +3,24 @@ import satData from "./satData";
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbit</button>
     </div>
   );
-}; 
+};
 
 export default Buttons;
+
+// {displaySats.map((sat, id) => {
+//   return (
+//     <button onClick={() => filterByType(sat)} key={id}>
+//       {sat} Orbit
+//     </button>
+//   );
+// })}
